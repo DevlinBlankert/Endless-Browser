41a0f10e9b6ee9828975305db50a73e8c41dc6d9
DevlinBlankert <devlin.blankert0827@mail.com>
2016-03-15 03:09:11 +0800

    endless-browser

---
+++ Endless/WebViewController.h
@@ -0,0 +1,38 @@
+/*
+ * Endless
+ * Copyright (c) 2014-2015 joshua stein <jcs@jcs.org>
+ *
+ * See LICENSE file for redistribution terms.
+ */
+
+#import <UIKit/UIKit.h>
+#import "IASKAppSettingsViewController.h"
+#import "WebViewTab.h"
+#import "WYPopoverController.h"
+
+@interface WebViewController : UIViewController <UITableViewDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate, UIScrollViewDelegate, IASKSettingsDelegate, WYPopoverControllerDelegate>
+
+@property BOOL toolbarOnBottom;
+@property BOOL darkInterface;
+
+- (NSMutableArray *)webViewTabs;
+- (__strong WebViewTab *)curWebViewTab;
+
+- (id)settingsButton;
+
+- (void)viewIsVisible;
+
+- (WebViewTab *)addNewTabForURL:(NSURL *)url;
+- (void)removeTab:(NSNumber *)tabNumber andFocusTab:(NSNumber *)toFocus;
+- (void)removeTab:(NSNumber *)tabNumber;
+- (void)removeAllTabs;
+
+- (void)webViewTouched;
+- (void)updateProgress;
+- (void)updateSearchBarDetails;
+- (void)refresh;
+- (void)forceRefresh;
+- (void)dismissPopover;
+- (void)prepareForNewURLFromString:(NSString *)url;
+
+@end
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, "7.0"
+
+pod "InAppSettingsKit"
+pod "WYPopoverController", :git => "https://github.com/sammcewan/WYPopoverController.git"
+pod "DTFoundation"
+pod "OrderedDictionary"
+pod "QuickDialog", :git => "https://github.com/jcs/QuickDialog.git"
+
+target "Endless Tests", :exclusive => true do
+	pod "OCMock"
+end
